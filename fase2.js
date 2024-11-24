da0b04f18f960331a9fa0a8c5d5077b250f1c77d
Jucivaldo00 <160550291+Jucivaldo00@users.noreply.github.com>
2024-11-24 14:16:14 -0300

    Create fase2.js

---
+++ fase2.js
@@ -0,0 +1,156 @@
+let questions = [
+
+    {
+
+        numb: 1,
+        question: "Dica: Refere-se a uma criança",
+        answer: "bebê",
+        options: [
+            "bêbe",
+            "bebê"
+        ],
+        hint: "A palavra se refere a uma criança."
+    },
+    {
+        numb: 2,
+        question: "Dica: É um determinado território social, político e geograficamente delimitado",
+        answer: "país",
+        options: [
+            "páis",
+            "país"
+        ],
+        hint: "Refere-se a determinado território social, política e geograficamente delimitado."
+    },
+    {
+        numb: 3,
+        question: "Dica: Aparelho destinado a produzir movimentos ou transformar determinada forma de energia",
+        answer: "máquina",
+        options: [
+            "máquina",
+            "maquína"
+        ],
+        hint: "Utilizada para facilitar o trabalho."
+    },
+    {
+        numb: 4,
+        question: "Dica: Se refere a atos ou movimentos'",
+        answer: "ações",
+        options: [
+            "acoés",
+            "ações"
+        ],
+        hint: "Se refere a atos ou movimentos."
+    },
+    {
+        numb: 5,
+        question: "Dica: Usado para medir o tempo",
+        answer: "relógio",
+        options: [
+            "relógio",
+            "rélogiô"
+        ],
+        hint: "Usado para medir o tempo."
+    },
+    {
+        numb: 6,
+        question: "Dica: Um grupo organizado de soldados",
+        answer: "exército",
+        options: [
+            "exercíto",
+            "exército"
+        ],
+        hint: "Um grupo organizado de soldados."
+    },
+    {
+        numb: 7,
+        question: "Dica: Uma bebida popular feita de grãos torrados",
+        answer: "café",
+        options: [
+            "café",
+            "cáfe"
+        ],
+        hint: "Uma bebida popular feita de grãos torrados."
+    },
+    {
+        numb: 8,
+        question: "Dica: Refere-se a algo que acontece em pouco tempo",
+        answer: "rápida",
+        options: [
+            "rapída",
+            "rápida"
+        ],
+        hint: "Refere-se a algo que acontece em pouco tempo."
+    },
+    {
+        numb: 9,
+        question: "Diva: Recurso natural fundamental para vida",
+        answer: "água",
+        options: [
+            "aguá",
+            "água",
+        ],
+        hint: "Essencial para a vida."
+    },
+    {
+        numb: 10,
+        question: "Dica: Utilizada para falar e comer",
+        answer: "língua",
+        options: [
+            "língua",
+            "lingua",
+        ],
+        hint: "Utilizada para falar e comer."
+    },
+    {
+        numb: 11,
+        question: "Dica: refere-se a uma coisa relacionada com o tórax",
+        answer: "toráxico",
+        options: [
+            "toráxico",
+            "tóraxico"
+        ],
+        hint: "é uma coisa relacionada com o tórax."
+    },
+    {
+        numb: 12,
+        question: "Dica: refere-se a uma vantagem ou direito concedido apenas a algumas pessoas",
+        answer: "privilégio",
+        options: [
+            "privilégio",
+            "privilegió"
+        ],
+        hint: "Vantagem ou direito concedido apenas a certas pessoas."
+    },
+    {
+        numb: 13,
+        question: "Dica: indica o principal órgão do sistema central",
+        answer: "cérebro",
+        options: [
+            "célebro",
+            "cérebro"
+        ],
+        hint: "Um dos principais órgãos do corpo."
+    },
+    {
+        numb: 14,
+        question: "Dica: colocar, botar algo em um lugar",
+        answer: "pôr",
+        options: [
+            "por",
+            "pôr"
+        ],
+        hint: "Colocar algo em algum lugar ou posição."
+    },
+    {
+        numb: 15,
+        question: "Dica: é muito usado para adoçar alimentos e bebidas",
+        answer: "açúcar",
+        options: [
+            "açucar",
+            "açúcar"
+        ],
+        hint: "Substância doce, geralmente em pó ou em cristais."
+    }
+];
+
+
